6b0ac132243175538e8ab25a47e237eaa7cc392d
MKnoles <mknoles78@gmail.com>
2020-10-02 23:52:43 -0400

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Jumbotron from './components/Jumbotron.jsx';
 import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Jumbotron />
     </div>
   );
 }
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Jumbotron = () => {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Welcome to the Grading Tool</h1>
+            <p class="lead">This tool is to assist in grading</p>
+        </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
