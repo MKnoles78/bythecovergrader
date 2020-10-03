dd69640d7aa4e510d69016f481bc0d993b8f69d3
MKnoles <mknoles78@gmail.com>
2020-10-02 23:47:55 -0400

    added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy" />
+            By the Cover Grading Tool
+          </a>
+        </nav>
+        </div>
+    );
+};
+
+export default Navbar;
