35a094ab9927755a5de5b5d8a9e83e3133ef5857
MKnoles <mknoles78@gmail.com>
2020-10-04 23:42:14 -0400

    added form input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Jumbotron from './components/Jumbotron.jsx';
 import Navbar from './components/Navbar.jsx'
-
+import Forms from './components/Forms'
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Jumbotron />
+      <Forms />
     </div>
   );
 }
