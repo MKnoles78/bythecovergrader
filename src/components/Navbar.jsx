ea68bcfe6d3bca9c4cd6dffc507df7f1a6c691b6
MKnoles <mknoles78@gmail.com>
2020-10-05 23:10:53 -0400

    updated spelling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav class="navbar navbar-light bg-light">
           <a class="navbar-brand" href="#">
             <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy" />
-            By the Cover
+            Buy the Cover
           </a>
         </nav>
         </div>
