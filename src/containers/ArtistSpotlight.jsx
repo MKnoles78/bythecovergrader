a2bac646565d1c7ea4020a6161e2bd6925652307
MKnoles <mknoles78@gmail.com>
2020-10-10 22:27:12 -0400

    added containers for pages

---
+++ src/containers/ArtistSpotlight.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArtistSpotlight extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ArtistSpotlight;
---
+++ src/containers/Grader.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Grader extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Grader;
