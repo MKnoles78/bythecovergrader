100435ee3eb649fa1ee0d1ec841964302d2346c6
MKnoles <mknoles78@gmail.com>
2020-10-15 22:26:22 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Jumbotron from './components/Jumbotron.jsx';
 import Navbar from './components/Navbar.jsx'
 import Forms from './components/Forms'
+
+
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+      <Switch>
       <Jumbotron />
       <Forms />
       <Forms />
+      </Switch>
+      </Router>
     </div>
   );
 }
